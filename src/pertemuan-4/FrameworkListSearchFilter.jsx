229f991913eff736c83519189587898802fda6e8
naswrrr <nasywa24si@mahasiswa.pcr.ac.id>
2026-04-17 16:08:40 +0700

    pertemuan 4: responsive design

--- src/pertemuan-4/FrameworkListSearchFilter.jsx
+++ src/pertemuan-4/FrameworkListSearchFilter.jsx
@@ -3,10 +3,26 @@ import frameworkData from "./framework.json";
 
 export default function FrameworkListSearchFilter() {
   /** Deklrasai state **/
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedTag, setSelectedTag] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const [selectedTag, setSelectedTag] = useState("");
 
-  const _searchTerm = searchTerm.toLowerCase();
+  /*Inisialisasi DataForm*/
+		const [dataForm, setDataForm] = useState({
+			searchTerm: "",
+			selectedTag: "",
+			/*Tambah state lain beserta default value*/
+			});
+		
+		/*Inisialisasi Handle perubahan nilai input form*/
+		const handleChange = (evt) => {
+			const { name, value } = evt.target;
+			setDataForm({
+				...dataForm,
+				[name]: value,
+			});
+		};
+
+  const _searchTerm = dataForm.searchTerm.toLowerCase();
   const filteredFrameworks = frameworkData.filter((framework) => {
     const matchesSearch =
       framework.name
@@ -16,11 +32,9 @@ export default function FrameworkListSearchFilter() {
         .toLowerCase()
         .includes(_searchTerm);
 
-    const matchesTag = selectedTag ? framework.tags.includes(selectedTag) : true;
+    const matchesTag = dataForm.selectedTag ? framework.tags.includes(dataForm.selectedTag) : true;
 
     return matchesSearch && matchesTag;
-
-
 
   });
   /** Deklarasi pengambilan unique tags di frameworkData **/
@@ -39,12 +53,13 @@ export default function FrameworkListSearchFilter() {
           name="searchTerm"
           placeholder="Search framework..."
           className="w-full p-2 border border-gray-300 rounded mb-4"
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleChange}
         />
 
         <select
           name="selectedTag"
           className="w-full p-2 border border-gray-300 rounded mb-4"
+          onChange={handleChange}
         >
           <option value="">All Tags</option>
           {allTags.map((tag, index) => (
